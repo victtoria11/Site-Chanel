f7fe5f67c8c6b91b34e2f05926fa2098ee6efe72
victtoria11 <victoriapachecolobo@gmail.com>
2023-08-09 14:05:11 -0300

    teste

--- projeto-site/src/BotaoFormulario.js
+++ projeto-site/src/BotaoFormulario.js
@@ -3,6 +3,7 @@ import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import FormularioCadastro from './FormularioCadastro'; // Importe o componente do formulário de cadastro
 import FormularioEntrar from './FormularioEntrar'; // Importe o componente do formulário de entrar
+import Typography from '@mui/material/Typography';
 
 
 
@@ -23,22 +24,23 @@ const Botao = () => {
 
     return (
         <Box sx={{ marginTop: 8, display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+            <Typography variant="h3">CONTA</Typography>
           <Box sx={{display: 'flex', flexDirection: 'row'}}>
-          <Button
-              onClick={handleMostrarFormularioCadastro}
-              sx={{
-                  mt: 3, mb: 2, borderBottom: mostrarFormularioCadastro ? '2px solid black' : '2px solid #3bbeff', borderRadius: 0,
-              }}
-          >
-              Cadastrar
-          </Button>
           <Button
               onClick={handleMostrarFormularioEntrar}
               sx={{
-                  mt: 3, mb: 2, borderBottom: mostrarFormularioEntrar ? '2px solid black' : '2px solid #3bbeff',  borderRadius: 0
+                  mt: 3, mb: 2, borderBottom: mostrarFormularioEntrar ? '3px solid black' : '1px solid #3bbeff',  borderRadius: 0
               }}
           >
               Entrar
+          </Button>
+          <Button
+              onClick={handleMostrarFormularioCadastro}
+              sx={{
+                  mt: 3, mb: 2, borderBottom: mostrarFormularioCadastro ? '3px solid black' : '1px solid #3bbeff', borderRadius: 0,
+              }}
+          >
+              Cadastrar
           </Button>
           </Box>
 
