23adc9e84dfc6dd0f6528f7f40647bf67b828177
ronaldmenezess <ronalddavi2508@gmail.com>
2023-08-14 13:39:45 -0300

    ok

--- projeto-site/server/db.js
+++ projeto-site/server/db.js
@@ -2,7 +2,7 @@ const { Pool } = require('pg')
 const cliente = new Pool({
     user: 'postgres',
     password: '123',
-    host: '150.162.182.249',
+    host: '150.162.183.138',
     port: 5432,
     database: 'Chanel',
 });
