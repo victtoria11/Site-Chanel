9dbd2a7e95ac2958160399ce1695159fa096eb34
victtoria11 <victoriapachecolobo@gmail.com>
2023-09-05 00:56:38 -0300

    ok

--- projeto-site/server/db.js
+++ projeto-site/server/db.js
@@ -2,7 +2,7 @@ const { Pool } = require('pg')
 const cliente = new Pool({
     user: 'postgres',
     password: '123',
-    host: '150.162.182.89',
+    host: '192.168.0.140',
     port: 5432,
     database: 'Chanel',
 });
--- projeto-site/src/componentes/Navbar.js
+++ projeto-site/src/componentes/Navbar.js
@@ -39,7 +39,7 @@ function Header(props) {
   };
 
   const handleLogin = () => {
-    setIsLoggedIn(true); 
+    setIsLoggedIn(false); 
     navigate('/');
   };
 
